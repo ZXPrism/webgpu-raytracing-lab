055c68f1c24a2c4b9069bbf7fcc6fde7fb6a6139
ZXPrism <2897544036@qq.com>
2025-11-29 19:52:08 +0800

    Simple ray casting

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import type { Kernel } from "./kernel";
 import { KernelBuilder } from "./kernel_builder";
 import { BindGroupBuilder } from "./bind_group_builder";
 
-import shader_render from './shaders/render.wgsl?raw';
+import shader_render from "./shaders/render.wgsl?raw";
 import { createGPUBuffer } from "./kernel_utils";
 
 let g_device!: GPUDevice;
@@ -21,7 +21,7 @@ async function init_webgpu() {
         return;
     }
 
-    const has_bgra8unorm_storage = adapter.features.has('bgra8unorm-storage');
+    const has_bgra8unorm_storage = adapter.features.has("bgra8unorm-storage");
     const device = await adapter.requestDevice({
         requiredLimits: {
             maxBufferSize: adapter.limits.maxBufferSize,
@@ -31,8 +31,8 @@ async function init_webgpu() {
             maxStorageBuffersPerShaderStage: adapter.limits.maxStorageBuffersPerShaderStage
         },
         requiredFeatures: has_bgra8unorm_storage
-            ? ['bgra8unorm-storage', 'subgroups'] as const
-            : ['subgroups'] as const,
+            ? ["bgra8unorm-storage", "subgroups"] as const
+            : ["subgroups"] as const,
     });
     if (!device) {
         console.error("failed to initialize WebGPU!");
@@ -42,7 +42,7 @@ async function init_webgpu() {
 
     const presentation_format = has_bgra8unorm_storage
         ? navigator.gpu.getPreferredCanvasFormat()
-        : 'rgba8unorm';
+        : "rgba8unorm";
 
     const canvas = document.querySelector("canvas")!;
     const context = canvas.getContext("webgpu");
@@ -69,30 +69,46 @@ async function init_webgpu() {
 }
 
 function init_kernels() {
-    const screen_info_buffer = createGPUBuffer(g_device, 'screen info', GPUBufferUsage.UNIFORM, 8);
+    const screen_info_buffer = createGPUBuffer(g_device, "screen info", GPUBufferUsage.UNIFORM, 8);
     const screen_info_data = new Uint32Array(2);
     screen_info_data[0] = g_canvas_width;
     screen_info_data[1] = g_canvas_height;
     g_device.queue.writeBuffer(screen_info_buffer, 0, screen_info_data);
 
-    g_render_kernel = new KernelBuilder(g_device, 'render_kernel', shader_render, 'compute')
+    const camera_info_buffer = createGPUBuffer(g_device, "camera info", GPUBufferUsage.UNIFORM, 48);
+    const camera_info_data = new Float32Array(10);
+    camera_info_data[0] = 0.0; // eye.x
+    camera_info_data[1] = 0.0; // eye.y
+    camera_info_data[2] = 1.0; // eye.z
+    camera_info_data[3] = 0.0; // (padding)
+    camera_info_data[4] = 0.0; // center.x
+    camera_info_data[5] = 0.0; // center.y
+    camera_info_data[6] = 0.0; // center.z
+    camera_info_data[7] = 1.0; // focal_length
+    camera_info_data[8] = 90.0 / 180.0 * Math.PI; // fov_y, in radians
+    camera_info_data[9] = g_canvas_width / g_canvas_height; // aspect_ratio
+    g_device.queue.writeBuffer(camera_info_buffer, 0, camera_info_data);
+
+    g_render_kernel = new KernelBuilder(g_device, "render_kernel", shader_render, "compute")
         .build();
-    g_render_kernel_bind_group = new BindGroupBuilder(g_device, 'render bind group')
-        .add_buffer('in_screen_info', 0, screen_info_buffer)
+    g_render_kernel_bind_group = new BindGroupBuilder(g_device, "render bind group")
+        .add_buffer("in_screen_info", 0, screen_info_buffer)
+        .add_buffer("in_camera", 1, camera_info_buffer)
+        .create_then_add_buffer("test_buffer", 2, GPUBufferUsage.STORAGE, g_canvas_width * g_canvas_height * 16)
         .build(g_render_kernel);
 }
 
 function render() {
     function _render(_time: DOMHighResTimeStamp) {
-        const framebuffer_bind_group = new BindGroupBuilder(g_device, 'framebuffer bind group')
-            .add_buffer('out_framebuffer', 0, g_context.getCurrentTexture().createView())
+        const framebuffer_bind_group = new BindGroupBuilder(g_device, "framebuffer bind group")
+            .add_buffer("out_framebuffer", 0, g_context.getCurrentTexture().createView())
             .build(g_render_kernel, 1);
 
         const command_encoder = g_device.createCommandEncoder();
         {
             g_render_kernel.dispatch_multiple_bind_group(command_encoder, [g_render_kernel_bind_group, framebuffer_bind_group],
-                Math.ceil(g_canvas_width / 32),
-                Math.ceil(g_canvas_height / 32),
+                Math.ceil(g_canvas_width / 16),
+                Math.ceil(g_canvas_height / 16),
                 1);
 
             g_device.queue.submit([command_encoder.finish()]);
--- src/shaders/render.wgsl
+++ src/shaders/render.wgsl
@@ -1,26 +1,68 @@
-struct ScreenInfo {
-  width: u32,
-  height: u32
+struct ScreenInfo { // 8
+  width: u32, // 0 -> 4
+  height: u32 // 4 -> 4
+}
+
+struct CameraInfo { // 40
+  eye: vec3f, // 0 -> 16 (12 + 4)
+  center: vec3f, // 16 -> 12
+  focal_length: f32, // 28 -> 4
+  fov_y: f32, // 32 -> 4
+  aspect_ratio: f32 // 36 -> 4
+}
+
+struct Ray { // 28
+  source: vec3f, // 0 -> 16
+  direction: vec3f, // 16 -> 12; not necessarily normalized
 }
 
 @group(0) @binding(0) var<uniform> in_screen_info: ScreenInfo;
+@group(0) @binding(1) var<uniform> in_camera_info: CameraInfo;
+@group(0) @binding(2) var<storage, read_write> out_test_buffer: array<vec4f>;
 
 // todo: should set format according to actual presentation format acquired from canvas..
 @group(1) @binding(0) var out_framebuffer : texture_storage_2d<bgra8unorm, write>;
 
 @compute
-@workgroup_size(32, 32, 1)
+@workgroup_size(16, 16, 1)
 fn compute(
   @builtin(global_invocation_id) global_id : vec3u
 ) {
   let x = global_id.x;
   let y = global_id.y;
 
-  let delta_x = f32(i32(x) - i32(in_screen_info.width / 2));
-  let delta_y = f32(i32(y) - i32(in_screen_info.height / 2));
+  let width = in_screen_info.width;
+  let height = in_screen_info.height;
+  let offset = y * width + x;
 
-  let pixel_out = select(vec3f(1.0), vec3f(1.0, 0.0, 0.0), delta_x * sin(delta_x * delta_x) + delta_y * cos(delta_y * delta_y) < 10.0);
-  if x < in_screen_info.width && y < in_screen_info.height {
+  // ========
+  //  camera
+  // ========
+  let camera_info = in_camera_info;
+  let camera_gaze_norm = normalize(camera_info.center - camera_info.eye);
+  let camera_right_norm = normalize(cross(camera_gaze_norm, vec3f(0.0, 1.0, 0.0)));
+  let camera_down_norm = cross(camera_gaze_norm, camera_right_norm);
+
+  // =============================
+  //  viewport (origin: top left)
+  // =============================
+  let viewport_height = 2 * tan(camera_info.fov_y / 2.0) * camera_info.focal_length;
+  let viewport_width = viewport_height * camera_info.aspect_ratio;
+  let viewport_u = camera_right_norm * viewport_width;
+  let viewport_v = camera_down_norm * viewport_height;
+  let viewport_u_base = viewport_u / f32(width);
+  let viewport_v_base = viewport_v / f32(height);
+  let viewport_upper_left = camera_info.eye + camera_gaze_norm * camera_info.focal_length - (viewport_u + viewport_v) / 2.0;
+  let pixel00 = viewport_upper_left + 0.5 * (viewport_u_base + viewport_v_base); // default sample point is the center of each pixel
+
+  // ================
+  //  ray generation
+  // ================
+  let ray = Ray(camera_info.eye, pixel00 + viewport_u_base * f32(x) + viewport_v_base * f32(y) - camera_info.eye);
+  let ray_direction_norm = normalize(ray.direction);
+
+  let pixel_out = abs(ray_direction_norm);
+  if x < width && y < height {
     textureStore(out_framebuffer, global_id.xy, vec4f(pixel_out, 1.0));
   }
 }
