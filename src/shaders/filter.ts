b2c9a8f249db4632239d655f5cbb04a3cdb88da0
ZXPrism <150247305+ZXPrism@users.noreply.github.com>
2026-09-23 21:33:38 +0800

    Remove subgroup

--- src/shaders/filter.ts
+++ src/shaders/filter.ts
@@ -17,29 +17,28 @@ var<workgroup> shared_render_diff: array<f32, WG_DIM_X>;
 fn compute(
   @builtin(workgroup_id) workgroup_id : vec3u,
   @builtin(local_invocation_index) thread_id: u32,
-  @builtin(subgroup_size) subgroup_size: u32,
-  @builtin(subgroup_id) subgroup_id: u32,
 ) {
   let id = (workgroup_id.x * WG_DIM_X) + thread_id;
   let n = in_scene_info.width * in_scene_info.height;
+
+  var render_diff = 0.0;
   if id < n {
     let curr = in_color_buffer[id];
     let prev = out_filtered_color_buffer[id];
     let filtered = mix(prev, curr, 1.0 / f32(in_frame_index));
-    let diff = filtered - out_filtered_color_buffer[id];
-    shared_render_diff[thread_id] = dot(diff, diff);
+
+    let diff = filtered - prev;
+    render_diff = dot(diff, diff);
+
     out_filtered_color_buffer[id] = filtered;
   }
+  shared_render_diff[thread_id] = render_diff;
 
   workgroupBarrier();
 
-  let is_first_lane = (thread_id % subgroup_size) == 0u;
-
-  for (var segment_cnt = WG_DIM_X; segment_cnt > 1u; segment_cnt = (segment_cnt + subgroup_size - 1) / subgroup_size) {
-    let value = select(0.0, shared_render_diff[thread_id], thread_id < segment_cnt);
-    let curr_warp_res = subgroupAdd(value);
-    if is_first_lane {
-      shared_render_diff[subgroup_id] = curr_warp_res;
+  for (var stride = WG_DIM_X / 2u; stride >= 1u; stride = stride / 2u) {
+    if thread_id < stride {
+      shared_render_diff[thread_id] += shared_render_diff[thread_id + stride];
     }
 
     workgroupBarrier();
